6f4e7cac412b8d636d3fffebfa9b5328ec8d9f5c
hblok <github@hblok.net>
2026-09-15 14:26:02 +0200

    flacs

--- tracks/dune/play_sandwerk.sh
+++ tracks/dune/play_sandwerk.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Plays the SANDWERK story arc in running order (see sandwerk_running_order.md).
 # Latest rendered version of each track. Run from /workspace/music.
-mplayer night_pursuit.wav the_maker_comes.wav kanly.wav water_of_life.wav the_sleeper_awakens.wav fall_of_arrakeen_v2.wav the_navigator.wav jihad.wav kwisatz_haderach.wav gurneys_song_v2.wav litany_against_fear.wav sihaya.wav
+mplayer night_pursuit.flac the_maker_comes.flac kanly.flac water_of_life.flac the_sleeper_awakens.flac fall_of_arrakeen_v2.flac the_navigator.flac jihad.flac kwisatz_haderach.flac gurneys_song_v2.flac litany_against_fear.flac sihaya.flac
--- tracks/trance/play_maschinenherz.sh
+++ tracks/trance/play_maschinenherz.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Plays the MASCHINENHERZ album in running order (see maschinenherz_running_order.md).
 # Latest rendered version of each track, from /workspace/music.
-mplayer tech_noir_v3.wav maschinenherz_v2.wav silver_wire_v3.wav morgenland_v3.wav flightpath.wav eisgang_v3.wav ungeschrieben.wav nachtkind_v3.wav penumbra.wav lost_v6.wav farlight_v2.wav adrift.wav
+mplayer tech_noir_v3.flac maschinenherz_v2.flac silver_wire_v3.flac morgenland_v3.flac flightpath.flac eisgang_v3.flac ungeschrieben.flac nachtkind_v3.flac penumbra.flac lost_v6.flac farlight_v2.flac adrift.flac
